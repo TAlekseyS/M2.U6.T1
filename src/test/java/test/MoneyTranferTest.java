97411ca4b083210847c13d2082ffb64ef9b612ce
Aleksey_T <talekseys@mail.ru>
2023-08-11 15:43:41 +0300

    ne rabotayet

---
+++ src/test/java/test/MoneyTranferTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.DashboardPage;
+import page.LoginPage;
+
+import static com.google.common.collect.Range.open;
+import static data.DataHelper.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTranferTest {
+
+    DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setup() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = getVerificationCode();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferFromFirstCardToSecond() {
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardInfo + amount;
+        var transferPage = dashboardPage.selectedCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+
+
+}
