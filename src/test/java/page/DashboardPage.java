22263ad6c88388dcd371cf1a85ca087c55ffdd88
Aleksey_T <talekseys@mail.ru>
2023-08-24 13:24:05 +0300

    Ubral lishiy probel

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -15,7 +15,7 @@ public class DashboardPage {
 
     private final String balanceStart = "баланс: ";
 
-    private final String balanceFinish = "р. ";
+    private final String balanceFinish = "р.";
 
     private final SelenideElement heading = $("[data-test-id=dashboard]");
 
